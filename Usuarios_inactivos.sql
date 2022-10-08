76deb6dfa89773df3d68754856e9b5a722a07d53
manuelmarchena <86371555+manuelmarchena@users.noreply.github.com>
2022-10-08 01:34:49 -0300

    Update Usuarios_inactivos.sql

--- Usuarios_inactivos.sql
+++ Usuarios_inactivos.sql
@@ -1,16 +1,16 @@
 CREATE DEFINER=`root`@`localhost` FUNCTION `Ususarios_inactivos`(p_DNI BIGINT, p_equipo VARCHAR(10)) RETURNS varchar(10) CHARSET utf8mb4
+    DETERMINISTIC
 BEGIN
-DECLARE v_dni BIGINT;
+DECLARE v_estado VARCHAR(10);
 
-SELECT nombre, apellido
-INTO v_dni
-FROM usuarios
+SELECT e.estado
+INTO v_estado
+FROM usuario AS u
  INNER JOIN
     estado AS e ON u.estado_id = e.id_estado
         INNER JOIN
     equipo AS eq ON eq.id_equipo = u.equipo_id
 WHERE
-    e.estado = 'Inactivo'
-        AND eq.nombre = p_equipo AND u.DNI= p_DNI;
-RETURN v_dni;
+	eq.nombre = p_equipo AND u.DNI= p_DNI;
+RETURN v_estado;
 END
