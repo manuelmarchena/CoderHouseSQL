922b33bb8272ad232f36d18d3720182650d98725
manuelmarchena <86371555+manuelmarchena@users.noreply.github.com>
2022-10-08 01:18:01 -0300

    Update documentos_demorados.sql

--- documentos_demorados.sql
+++ documentos_demorados.sql
@@ -1,8 +1,8 @@
 CREATE DEFINER=`root`@`localhost` FUNCTION `documento_demorado`(p_nombre_documento VARCHAR(100)) RETURNS varchar(60) CHARSET utf8mb4
+    DETERMINISTIC
 BEGIN
 
 DECLARE v_documento VARCHAR(60);
-
 
 SELECT DISTINCT 
     d.documento
@@ -13,7 +13,8 @@ FROM
     prioridad AS p ON d.prioridad_id = p.id_prioridad
 WHERE
     p.prioridad = 'Alta'
-        AND DATE_ADD(d.fecha_creacion,
-        INTERVAL 180 DAY)< '2022-07-01';
+       AND DATE_ADD(d.fecha_creacion,
+       INTERVAL 180 DAY)< '2022-07-01'
+        AND d.nombre_doc = p_nombre_documento;
 RETURN v_documento;
 END
