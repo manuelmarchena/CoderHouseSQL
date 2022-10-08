7fec230cc2858a0f641b45daecf7e270d10932fe
manuelmarchena <86371555+manuelmarchena@users.noreply.github.com>
2022-10-08 00:37:34 -0300

    Update ordenar_por.sql

--- ordenar_por.sql
+++ ordenar_por.sql
@@ -1,11 +1,6 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `order_by`(IN p_tabla VARCHAR(20), IN p_criterio varchar(20), IN p_orden varchar(4))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `order_by`(IN p_tabla VARCHAR(20), IN p_columna varchar(20), IN p_orden varchar(4))
 BEGIN
-SET @tabla = p_tabla;
-SET @criterio = p_criterio;
-SET @orden = p_orden;
-SET @consulta = 'SELECT * FROM '+
-	@tabla+ ' ORDER BY( '+ @criterio+') ' + @orden;
-PREPARE stmt FROM @consulta ;
+SET @consulta = CONCAT('SELECT * FROM ',p_tabla, ' ORDER BY ', p_columna, ' ', p_orden);
+PREPARE stmt FROM @consulta;
 EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
 END
