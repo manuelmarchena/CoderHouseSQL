0595a2f189639df4e00232ff0d8faa40401ebc71
manuelmarchena <86371555+manuelmarchena@users.noreply.github.com>
2022-10-10 22:36:17 -0300

    Update log_borrados.sql

--- log_borrados.sql
+++ log_borrados.sql
@@ -1,4 +1,5 @@
 CREATE DEFINER=`root`@`localhost` TRIGGER `proyecto_final_sql`.`log_borrados` BEFORE DELETE ON `documento` FOR EACH ROW
 BEGIN
 INSERT INTO old_records VALUES (NULL, OLD.id_documento, CURRENT_DATE(), CURRENT_TIME(), OLD.usuario_id, OLD.documento, OLD.proyecto_id);
+-- Guarda una copia de los documentos que son eliminados de las DB
 END
