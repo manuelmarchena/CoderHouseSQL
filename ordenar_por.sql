2cde195dac6d635d42d7161e8ce514dc608c274e
manuelmarchena <86371555+manuelmarchena@users.noreply.github.com>
2022-10-22 15:39:24 -0300

    Update and rename ordenar_por.sql to Proyectos-por-usuarios

--- ordenar_por.sql
+++
@@ -1,6 +0,0 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `order_by`(IN p_tabla VARCHAR(20), IN p_columna varchar(20), IN p_orden varchar(4))
-BEGIN
-SET @consulta = CONCAT('SELECT * FROM ',p_tabla, ' ORDER BY ', p_columna, ' ', p_orden);
-PREPARE stmt FROM @consulta;
-EXECUTE stmt;
-END
