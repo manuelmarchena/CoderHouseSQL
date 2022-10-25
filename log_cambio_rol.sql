af24682871d395a97069309078ae0de3df67293b
manuelmarchena <86371555+manuelmarchena@users.noreply.github.com>
2022-10-24 21:48:31 -0300

    Update log_cambio_rol.sql

--- log_cambio_rol.sql
+++ log_cambio_rol.sql
@@ -1,7 +1,7 @@
 CREATE DEFINER=`root`@`localhost` TRIGGER `proyecto_final_sql`.`log_cambio_rol` AFTER UPDATE ON `usuario` FOR EACH ROW
 BEGIN
 
-INSERT INTO cambio_rol VALUES(NULL, CURRENT_USER(), OLD.id_usuario, OLD.rol_id, NEW.rol_id);
+INSERT INTO log_cambio_rol VALUES(NULL, CURRENT_USER(), OLD.id_usuario, OLD.rol_id, NEW.rol_id, CURRENT_DATE(), CURRENT_TIME());
 -- Este trigger registra los cambios de rol realizados, indicando quien realizó el cambio de rol, cuando lo hizo y el rol anterior del usuario. 
 
 END
