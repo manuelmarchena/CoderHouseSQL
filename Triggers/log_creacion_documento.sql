d1988923adc9418bc9b106f1f512863d057e6720
manuelmarchena <86371555+manuelmarchena@users.noreply.github.com>
2022-10-25 17:20:16 -0300

    Add files via upload

--- Triggers/log_creacion_documento.sql
+++
@@ -1,5 +0,0 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER `proyecto_final_sql`.`log_creación_documento` AFTER INSERT ON `documento` FOR EACH ROW
-BEGIN
-INSERT INTO log_documentos VALUES(NULL, NEW.id_documento, NEW.usuario_id, CURRENT_DATE(), CURRENT_TIME()); 
--- Guarda un registro de creación de los documentos en el orden en que son creados
-END
