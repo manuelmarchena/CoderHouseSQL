8ed4edb4ecd405f4e4a3b9caa353f1d3cbd777f2
manuelmarchena <86371555+manuelmarchena@users.noreply.github.com>
2022-10-24 21:54:23 -0300

    Delete Triggers directory

--- Triggers/corrige_fecha.sql
+++
@@ -1,7 +0,0 @@
-CREATE DEFINER=`root`@`localhost` TRIGGER `proyecto_final_sql`.`corrige_fecha` BEFORE UPDATE ON `usuario` FOR EACH ROW
-BEGIN
-UPDATE usuario
-SET fecha_modificacion = CURRENT_DATE()
-WHERE fecha_modificación = '0000-00-00 00:00:00';
--- Modifica la fecha cuando se cargó de forma erronea
-END
